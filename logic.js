5cf4a4c71d28c21e95a4ab77f6ad4e21359da2af
Domingos João Kissimbila <95512079+Kissimbila16@users.noreply.github.com>
2023-11-25 16:26:40 +0100

    upload
    upload

---
+++ logic.js
@@ -0,0 +1,9 @@
+start=()=>{
+       var body=document.body;
+           body.style.animationPlayState="running";
+}
+
+para=()=>{
+    var body=document.body;
+        body.style.animationPlayState="paused";
+}
